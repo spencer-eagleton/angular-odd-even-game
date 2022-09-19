7151e86b40d65bb5a9c5447797f8e43a64eb8f2b
Spencer Eagleton <seagleton@basspro.com>
2022-09-19 15:18:30 -0500

    initial commit and start button starts interval

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber);
+  }
+  title = 'angular-odd-even';
+}
